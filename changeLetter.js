0c30a4093039ef13ab15bb43b0be4b1ec37c783d
faunhub <76247482+faunhub@users.noreply.github.com>
2023-03-24 17:22:04 +0700

    added warning lline

--- changeLetter.js
+++ changeLetter.js
@@ -34,11 +34,3 @@ for (let i=0; i < letterCollection.length; i++){
 
 changeBtn.addEventListener('click', changeAll);
 let toggle = false;
-
-document.onreadystatechange = function() {
-    if (document.readyState !== "complete"){
-        document.querySelector('.loading').style.visibility = "visible";
-    } else {
-        document.querySelector('.loading').style.visibility = "hidden";
-    }
-}
